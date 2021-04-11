0391f4863a83dd7f3be700147f785fb148957199
taurin14 <neven.pesut@gmail.com>
2021-04-11 17:24:23 +0200

    4th commit

--- src/links/utils.py
+++ src/links/utils.py
@@ -2,6 +2,7 @@ import requests
 import lxml
 from bs4 import BeautifulSoup
 
+
 def get_link_data(url):
     headers = {
         "user-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
