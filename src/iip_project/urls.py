a2778964cf5fdcfe9dbff9949cd45a3ee8bde335
taurin14 <neven.pesut@gmail.com>
2021-04-11 20:07:19 +0200

    mozda

--- src/iip_project/urls.py
+++ src/iip_project/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('delete/<pk>', LinkDeleteView.as_view(), name="delete"),
 
 
-    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
